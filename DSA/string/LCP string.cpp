c3c4cb115c96359d282900060c14f09c6998afca
sum of subarray k equal <cse23026@glbitm.ac.in>
2026-03-29 15:36:39 +0530

    group anagram

--- DSA/string/LCP string.cpp
+++ DSA/string/LCP string.cpp
@@ -1,1 +1,17 @@
- 
+ class Solution {
+public:
+    string longestCommonPrefix(vector<string>& strs) {
+        sort(strs.begin(),strs.end());
+        // [" flight","flow","flower" ]
+        string first=strs[0]; // point = flight
+        string last=strs[strs.size()-1]; // point= last 
+        string ans="";
+        for(int i=0;i<first.size();i++){
+            if(first[i]!=last[i]){
+                break;
+            }
+            ans+=first[i];
+        }
+        return ans;
+    }
+}; 
