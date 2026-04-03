7ab11f54ca63f9adbe301ef00d900cf684fc4908
sum of subarray k equal <cse23026@glbitm.ac.in>
2026-04-03 22:33:41 +0530

    insertposition

--- DSA/Binary Search/lowerbound.cpp
+++ DSA/Binary Search/lowerbound.cpp
@@ -1,53 +1,17 @@
-class Solution {
-public:
-    int lowerBound(vector<int>& nums, int target) {
-        int n = nums.size();
-        int start = 0, end = n - 1;
-        int ans = n;
+int lowerBound(vector<int>& nums, int target) {
+    int n = nums.size();
+    int start = 0, end = n - 1;
+    int ans = n;   // default (agar > target na mile)
 
-        while (start <= end) {
-            int mid = start + (end - start) / 2;
+    while (start <= end) {
+        int mid = start + (end - start) / 2;
 
-            if (nums[mid] >= target) {
-                ans = mid;
-                end = mid - 1;
-            } else {
-                start = mid + 1;
-            }
+        if (nums[mid] >=target) {
+            ans = mid;        // possible answer
+            end = mid - 1;    // left mein aur chhota index dhundo
+        } else {
+            start = mid + 1;  // right side jao
         }
-        return ans;
     }
-
-    int upperBound(vector<int>& nums, int target) {
-        int n = nums.size();
-        int start = 0, end = n - 1;
-        int ans = n;
-
-        while (start <= end) {
-            int mid = start + (end - start) / 2;
-
-            if (nums[mid] > target) {
-                ans = mid;
-                end = mid - 1;
-            } else {
-                start = mid + 1;
-            }
-        }
-        return ans;
-    }
-
-    vector<int> searchRange(vector<int>& nums, int target) {
-        int n = nums.size();
-
-        int lb = lowerBound(nums, target);
-
-        // target not found
-        if (lb == n || nums[lb] != target) {
-            return {-1, -1};
-        }
-
-        int ub = upperBound(nums, target);
-
-        return {lb, ub - 1};
-    }
-};
+    return ans;
+}
---
+++ DSA/Binary Search/upperbound.cpp
@@ -0,0 +1,17 @@
+int upperBound(vector<int>& nums, int target) {
+    int n = nums.size();
+    int start = 0, end = n - 1;
+    int ans = n;   // default (agar > target na mile)
+
+    while (start <= end) {
+        int mid = start + (end - start) / 2;
+
+        if (nums[mid] > target) {
+            ans = mid;        // possible answer
+            end = mid - 1;    // left mein aur chhota index dhundo
+        } else {
+            start = mid + 1;  // right side jao
+        }
+    }
+    return ans;
+}
