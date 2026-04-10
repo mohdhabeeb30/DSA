47ab9654802f3834487e31fe4eb5cabaae3e053d
sum of subarray k equal <cse23026@glbitm.ac.in>
2026-04-10 16:17:00 +0530

    maximumXorProduct

---
+++ DSA/bit manipulation/ Find the XOR of Numbers Which Appear Twice.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int duplicateNumbersXOR(vector<int>& nums) {
+        int n=nums.size();
+        int ans=0;
+        sort(nums.begin(),nums.end());
+        for(int i=0;i<n;i++){
+            if(i<n-1 && (nums[i]==nums[i+1])){
+                 ans^=nums[i];
+                 i++;
+            }
+        }
+        return ans;
+    }
+};
