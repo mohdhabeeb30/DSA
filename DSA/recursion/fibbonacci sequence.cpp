0563b08bb2d45aabde778b148eae9f61539b8d1e
sum of subarray k equal <cse23026@glbitm.ac.in>
2026-04-14 18:10:41 +0530

    GCD

---
+++ DSA/recursion/fibbonacci sequence.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    cout<<"Enter the number of terms: ";
+    cin>>n;
+
+    int a=0, b=1, c;
+    cout<<"Fibonacci Sequence: ";
+    for(int i=1; i<=n; i++){
+        cout<<a<<" ";
+        c = a + b;
+        a = b;
+        b = c;
+    }
+    return 0;
+}
