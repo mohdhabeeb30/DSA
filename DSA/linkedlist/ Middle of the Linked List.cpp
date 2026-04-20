667040eb66358558605f66ce4246da61e76ddf21
sum of subarray k equal <cse23026@glbitm.ac.in>
2026-04-20 22:33:45 +0530

    reverse

---
+++ DSA/linkedlist/ Middle of the Linked List.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    ListNode* middleNode(ListNode* head) {
+        ListNode* slow = head;
+        ListNode* fast = head;
+
+        while(fast != NULL && fast->next != NULL){
+            slow = slow->next;         // 1 step
+            fast = fast->next->next;   // 2 step
+        }
+
+        return slow;
+    }
+};
