a50613a4c6fe25f819adcc86948874b5b85cd814
sum of subarray k equal <cse23026@glbitm.ac.in>
2026-04-21 23:23:10 +0530

    remove nth from end

---
+++ DSA/linkedlist/find lenth of linkedlist.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int getLength(ListNode* head) {
+        ListNode* temp=head;
+        int count=0;
+        while(temp!=NULL){
+            count++;
+            temp=temp->next;
+        }
+        return count;
+    }
+};
