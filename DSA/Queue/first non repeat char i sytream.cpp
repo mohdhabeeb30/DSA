9945e4ac97379dfd5994544cc9b3e07e934f184e
sum of subarray k equal <cse23026@glbitm.ac.in>
2026-07-10 21:13:35 +0530

    level order

---
+++ DSA/Queue/first non repeat char i sytream.cpp
@@ -0,0 +1,4 @@
+// 
+// approach using queue and array
+// agar charter ka count >1 hai to  reparted ++ kro 
+// and check kro ki wo queue ke front me hai ya nhi agar hai to pop kro and check kro ki wo count 1 hai ya nhi
