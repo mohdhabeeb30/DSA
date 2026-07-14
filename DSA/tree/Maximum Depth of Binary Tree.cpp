c2e0ce89a4f028cb0ab90c4956c1f961c0e11c27
sum of subarray k equal <cse23026@glbitm.ac.in>
2026-07-14 11:55:18 +0530

    is balanced

--- DSA/tree/Maximum Depth of Binary Tree.cpp
+++ DSA/tree/Maximum Depth of Binary Tree.cpp
@@ -28,4 +28,16 @@ public:
     }
 };
   // 2nd approach using recursion
-  
+  class Solution {
+public:
+    int maxDepth(TreeNode* root) {
+        if (root == nullptr) {
+            return 0;
+        }
+        
+        int leftDepth = maxDepth(root->left);
+        int rightDepth = maxDepth(root->right);
+        
+        return max(leftDepth, rightDepth) + 1;
+    }
+};
